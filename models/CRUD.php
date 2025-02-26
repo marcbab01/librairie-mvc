bbb01da34d4ff11ebbb30cea60115caa89d108cc
marcbab01 <mobabin1993@gmail.com>
2025-02-26 09:43:11 -0500

    Commit 11 - CRUD.php : creation de la fonction select()

--- models/CRUD.php
+++ models/CRUD.php
@@ -6,4 +6,13 @@ class CRUD extends \PDO {
         parent::__construct('mysql:host=localhost; dbname=librairie_db; port=3306; charset=utf8', 'root', '');
 
     }
+
+    final public function select($field = null, $order = 'ASC') {
+        if($field == null) {
+            $field = this->primaryKey;
+        }
+        $sql = "SELECT * FROM $table ORDER BY $field $order";
+        $stmt = $this->query($sql);
+        return $stmt->fetchAll();
+    }
 }
