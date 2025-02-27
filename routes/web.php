520267e38c49440aab82934f6211fa25ed5f5ed4
marcbab01 <mobabin1993@gmail.com>
2025-02-26 23:04:59 -0500

    Commit 24 - Update routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -9,13 +9,21 @@ Route::get('/contact', 'HomeController@contact');
 Route::get('/product', 'HomeController@product');
 
 
-Route::get('/membre', 'MembreController@index');
+Route::get('/membres', 'MembreController@index');
 Route::get('/membre/create', 'MembreController@create');
 Route::post('/membre/create', 'MembreController@store');
 Route::get('/membre/show', 'MembreController@show');
 Route::get('/membre/edit', 'MembreController@edit');
 Route::post('/membre/edit', 'MembreController@update');
 Route::post('/membre/delete', 'MembreController@delete');
+
+Route::get('/livres', 'LivreController@index');
+Route::get('/livre/create', 'LivreController@create');
+Route::post('/livre/create', 'LivreController@store');
+Route::get('/livre/show', 'LivreController@show');
+Route::get('/livre/edit', 'LivreController@edit');
+Route::post('/livre/edit', 'LivreController@update');
+Route::post('/livre/delete', 'LivreController@delete');
 
 
 Route::dispatch();
