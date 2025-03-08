e1e5c7c09550d04a9025485a911021ba9a8aed5d
marcbab01 <mobabin1993@gmail.com>
2025-03-08 13:41:00 -0500

    Update CRUD.php - Ajout de la fonction unique()

--- models/CRUD.php
+++ models/CRUD.php
@@ -89,4 +89,17 @@ abstract class CRUD extends \PDO {
             return false;
         }
     }
+
+    final public function unique($field, $value) {
+        $sql = "SELECT * FROM $this->table WHERE $field = :$field";
+        $stmt = $this->prepare($sql);
+        $stmt->bindValue(":$field", $value);
+        $stmt->execute();
+        $count = $stmt->rowCount();
+        if($count == 1) {
+            return $stmt->fetch();
+        } else {
+            return false;
+        }
+    }
 }
